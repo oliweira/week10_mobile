3c11276b3c5a13d8774d2b29b16b38171ceb5950
Oliweira <oliweira@gmail.com>
2020-03-13 17:47:38 -0300

    Versionando os arquivos existentes

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Olá Oliweira!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  title: {
+    fontSize:30,
+    fontWeight: 'bold',
+    color: '#5dbcd2'
+  }
+});

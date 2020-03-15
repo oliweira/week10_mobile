ff89cbdd1493ce69a1cc416691427533315aa1fc
Oliweira <oliweira@gmail.com>
2020-03-15 15:56:47 -0300

    Criando o Main e Profile

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Olá Oliweira!</Text>
-    </View>
+    <Routes />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  title: {
-    fontSize:30,
-    fontWeight: 'bold',
-    color: '#5dbcd2'
-  }
-});

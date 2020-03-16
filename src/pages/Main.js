3cc441230e617cdf3893025f1cc823b77575f0b9
Oliweira <oliweira@gmail.com>
2020-03-16 15:29:59 -0300

    Adicionando o text e o button para buscar dos devs.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Image, View, Text } from 'react-native';
+import { StyleSheet, Image, View, Text, TextInput, TouchableOpacity, Keyboard } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+import { MaterialIcons } from '@expo/vector-icons';
 
 function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
@@ -35,20 +36,34 @@ function Main({ navigation }) {
     }
 
     return(
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -15.8291501, longitude: -47.9811394}}>
-                <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4' }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'oliweira' });
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Rafael Leite</Text>
-                        <Text style={styles.devBio}>Minha BIO</Text>
-                        <Text style={styles.devTechs}>PHP, JS, REACTJS</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -15.8291501, longitude: -47.9811394}}>
+                    <Image style={styles.avatar} source={{uri: 'https://avatars2.githubusercontent.com/u/1918147?s=400&u=3fce39c0f932ae979e2280940ff50dc811c7ff5c&v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'oliweira' });
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Rafael Leite</Text>
+                            <Text style={styles.devBio}>Minha BIO</Text>
+                            <Text style={styles.devTechs}>PHP, JS, REACTJS</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput 
+                    style={styles.searchInput}
+                    placeholder="buscar devs por techs..."
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+                <TouchableOpacity onPress={() => {}} style={styles.loadButton}>
+                    <MaterialIcons name="my-location" size={20} color='#FFF' />
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
@@ -76,6 +91,39 @@ const styles = StyleSheet.create({
     },
     devTechs: {
         marginTop:5
+    },
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4,
+        },
+        elevation: 2,
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4Dff',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
     },
 })
 
